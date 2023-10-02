e9fc0688a250136d43b7c570632ebf41c75b4f56
Delta-do <pumponisharf@mail.ru>
2023-10-02 15:12:16 +0300

    конец

--- cs_lab2/Form5.Designer.cs
+++ cs_lab2/Form5.Designer.cs
@@ -36,6 +36,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -45,7 +46,7 @@
             this.label1.Location = new System.Drawing.Point(38, 41);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(258, 28);
+            this.label1.Size = new System.Drawing.Size(357, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Заполните поля структуры";
             // 
@@ -54,7 +55,7 @@
             this.textBoxnum.Location = new System.Drawing.Point(56, 119);
             this.textBoxnum.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxnum.Name = "textBoxnum";
-            this.textBoxnum.Size = new System.Drawing.Size(170, 34);
+            this.textBoxnum.Size = new System.Drawing.Size(170, 45);
             this.textBoxnum.TabIndex = 1;
             this.textBoxnum.Text = "Номер";
             this.textBoxnum.TextChanged += new System.EventHandler(this.textBoxnum_TextChanged);
@@ -64,7 +65,7 @@
             this.textBoxmodel.Location = new System.Drawing.Point(56, 181);
             this.textBoxmodel.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxmodel.Name = "textBoxmodel";
-            this.textBoxmodel.Size = new System.Drawing.Size(170, 34);
+            this.textBoxmodel.Size = new System.Drawing.Size(170, 45);
             this.textBoxmodel.TabIndex = 2;
             this.textBoxmodel.Text = "Модель";
             this.textBoxmodel.TextChanged += new System.EventHandler(this.textBoxmodel_TextChanged);
@@ -74,7 +75,7 @@
             this.textBoxowner.Location = new System.Drawing.Point(56, 249);
             this.textBoxowner.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxowner.Name = "textBoxowner";
-            this.textBoxowner.Size = new System.Drawing.Size(170, 34);
+            this.textBoxowner.Size = new System.Drawing.Size(170, 45);
             this.textBoxowner.TabIndex = 3;
             this.textBoxowner.Text = "Владелец";
             this.textBoxowner.TextChanged += new System.EventHandler(this.textBoxowner_TextChanged);
@@ -84,7 +85,7 @@
             this.label_err.AutoSize = true;
             this.label_err.Location = new System.Drawing.Point(233, 107);
             this.label_err.Name = "label_err";
-            this.label_err.Size = new System.Drawing.Size(239, 56);
+            this.label_err.Size = new System.Drawing.Size(335, 76);
             this.label_err.TabIndex = 5;
             this.label_err.Text = "Некорректное значение.\nВведите другое.";
             // 
@@ -96,6 +97,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Вывести результат";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // label2
             // 
@@ -115,12 +117,24 @@
             this.button2.TabIndex = 10;
             this.button2.Text = "Возврат к 1 заданию >";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(676, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 38);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 38F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1100, 630);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
@@ -148,5 +162,6 @@
         private Button button1;
         private Label label2;
         private Button button2;
+        private Label label3;
     }
 }
--- cs_lab2/Form5.cs
+++ cs_lab2/Form5.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,16 +12,22 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace cs_lab2
 {
+    struct Car
+    {
+        public int num;
+        public string model;
+        public string owner;
+    }
     public partial class Form5 : Form
     {
         public Form5()
         {
             InitializeComponent();
+            this.Text = "2 Задание";
         }
 
         private void textBoxnum_TextChanged(object sender, EventArgs e)
         {
-            label_res.Visible = false;
             int v;
             if (int.TryParse(textBoxnum.Text, out v))
                 label_err.Visible = false;
@@ -38,15 +45,25 @@ namespace cs_lab2
             
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Car car = new Car();
+            if (int.TryParse(textBoxnum.Text, out car.num))
+                label_err.Visible = false;
+            else
+            {
+                car.num = 0;
+                label_err.Visible = true;
+            }
+            car.model = textBoxmodel.Text;
+            car.owner = textBoxowner.Text;
+            MessageBox.Show($"Номер машины: {car.num}\nМодель машины: {car.model}\nВладелец машины: {car.owner}");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
         {
             Form1 form = new Form1();
             form.Show();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
